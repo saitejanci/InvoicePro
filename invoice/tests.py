7345be2fff2325034c837cb0ad75e484d25453e3
saitejanci <x23411520@student.ncirl.ie>
2025-04-09 01:50:57 +0100

    changes
    changes

--- invoice/tests.py
+++ invoice/tests.py
@@ -6,3 +6,11 @@ class TestCase(TestCase):
         """Test if login page returns 200 status"""
         response = self.client.get(reverse('home'))
         self.assertEqual(response.status_code, 200)
+    def test_view_invoice_page_loads(self):
+        """Test if view_invoice page returns 200 status"""
+        response = self.client.get(reverse('view_invoice'))
+        self.assertEqual(response.status_code, 200)
+    def test_view_invoice_uses_correct_template(self):
+        """Test if correct template is used for invoice page"""
+        response = self.client.get(reverse('view_invoice'))
+        self.assertTemplateUsed(response, 'invoice/view_invoice.html')
